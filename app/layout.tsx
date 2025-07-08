f067644b9cfe03053547aa0c101b74fabfab74ca
streetcoder <streetkoder@gmail.com>
2025-07-07 23:50:00 -0600

    implement navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -19,7 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>{children}</body>
+      <body className={`${bricolage.variable} antialiased`}>
+        <Navbar />
+        {children}
+        </body>
     </html>
   );
 }
